55f704aa0b637a8aae3a7b7ff3e9d57e4b87b1f3
sirlocust <jua56yu@hotmail.com>
2020-08-04 15:46:09 -0500

    segunda clase del curso haciendo una caja registradora

--- src/team/ed/storage/DataBase.java
+++ src/team/ed/storage/DataBase.java
@@ -5,9 +5,14 @@ import team.ed.objects.Potato;
 import team.ed.objects.Product;
 import team.ed.objects.Rice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataBase {
 
     private final  Product[] products;
+    private List<Product> purchases;
+    private List<Product> sales;
 
     public DataBase(){
         this.products = new Product[3];
@@ -18,6 +23,9 @@ public class DataBase {
         products[0] = potato;
         products[1] = rice;
         products[2] = meat;
+
+        this.purchases = new ArrayList<>();
+        this.sales = new ArrayList<>();
     }
 
     public Product getByIndex(int index){
@@ -25,9 +33,78 @@ public class DataBase {
             System.out.println("indice no valido!");
             return null;
         }
-        return this.products[index];
+        Product result = null;
+        try {
+            result = products[index].clone();
+        }
+        catch (CloneNotSupportedException error){
+            System.out.println(error);
+        }
+        return result;
     }
     public Product[] getAll(){
-        return products;
+        Product[] result = new Product[3];
+        try {
+            result[0] = products[0].clone();
+            result[1] = products[1].clone();
+            result[2] = products[2].clone();
+        }
+        catch (CloneNotSupportedException error){
+            System.err.println(error);
+
+        }
+        return result;
+    }
+
+    public void buy(Product product){
+        Product temp;
+        switch (product.getClass().getSimpleName()){
+            case "Potato":
+                temp = products[0];
+                break;
+            case "Rice":
+                temp = products[1];
+                break;
+            case "Meat":
+                temp = products[2];
+                break;
+            default:
+                System.out.println("no valido");
+                return;
+        }
+
+        temp.setAmount(temp.getAmount()+ product.getAmount());
+        temp.setPrice(product.getPrice());
+        purchases.add(product);
+    }
+    public void sale(Product product){
+        Product temp;
+        switch (product.getClass().getSimpleName()){
+            case "Potato":
+                temp = products[0];
+                break;
+            case "Rice":
+                temp = products[1];
+                break;
+            case "Meat":
+                temp = products[2];
+                break;
+            default:
+                System.out.println("no valido");
+                return;
+        }
+
+        temp.setAmount(temp.getAmount() - product.getAmount());
+        product.setPrice(temp.getPrice() * 1.25f);
+        purchases.add(product);
+        sales.add(product);
+    }
+
+    public List<Product> getPurchases() {
+        return purchases;
+    }
+
+    public List<Product> getSales() {
+        return sales;
     }
 }
---
+++ src/team/ed/test/Persona.java
@@ -0,0 +1,6 @@
+package team.ed.test;
+
+public class Persona {
+    public String nombre;
+    public int edad;
+}
